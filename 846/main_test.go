a965b5a770d181bd7c07bbe94133a0e77a82e4b4
jinleileiking <jinleileiking@gmail.com>
2018-11-26 19:08:16 +0800

    846 55 / 65 test cases passed. Another solution, but slow too

--- 846/main_test.go
+++ 846/main_test.go
@@ -6,15 +6,15 @@ import (
 	"github.com/davecgh/go-spew/spew"
 )
 
-func TestGetW(t *testing.T) {
+// func TestGetW(t *testing.T) {
 
-	spew.Dump(getW([]int{4, 3, 3, 2, 2, 1}, 3))
-}
+// 	spew.Dump(getW([]int{4, 3, 3, 2, 2, 1}, 3))
+// }
 
-func TestGetW1(t *testing.T) {
+// func TestGetW1(t *testing.T) {
 
-	spew.Dump(getW([]int{3, 2, 1}, 3))
-}
+// 	spew.Dump(getW([]int{3, 2, 1}, 3))
+// }
 
 func TestMain(t *testing.T) {
 
@@ -29,4 +29,38 @@ func TestMain1(t *testing.T) {
 func TestMain2(t *testing.T) {
 
 	spew.Dump(isNStraightHand([]int{1, 2, 3}, 1))
+}
+func TestMain3(t *testing.T) {
+
+	spew.Dump(isNStraightHand([]int{1, 2, 2, 3, 3, 3, 4, 4, 5}, 2))
+}
+
+func TestHasw(t *testing.T) {
+
+	spew.Dump(hasW([]int{1, 2, 3, 4}, 3, 4))
+}
+
+func TestHasw1(t *testing.T) {
+
+	spew.Dump(hasW([]int{3, 3, 3, 1}, 2, 3))
+}
+
+func TestHasw2(t *testing.T) {
+
+	spew.Dump(hasW([]int{3, 2, 1}, 2, 3))
+}
+
+func TestCutw(t *testing.T) {
+
+	spew.Dump(cutW([]int{1, 2, 2, 1, 3, 4}, 3, 4))
+}
+
+func TestCutw1(t *testing.T) {
+
+	spew.Dump(cutW([]int{4, 3, 3, 3, 2, 2, 1}, 2, 4))
+}
+
+func TestCutw3(t *testing.T) {
+
+	spew.Dump(cutW([]int{3, 2, 1}, 2, 3))
 }
