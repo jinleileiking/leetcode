eed040ddcaf317343b2cb954d0af6612f1ab0d2e
jinleileiking <jinleileiking@gmail.com>
2019-02-27 14:25:02 +0800

    907: the clue have a buggie, progressing..

--- 907_sum-of-subarray-minimums/main.go
+++ 907_sum-of-subarray-minimums/main.go
@@ -1,41 +1,84 @@
 package main
 
-func minAndModify(arr []int) (int, int, []int) {
-	var min, minK int
+func mins(arr []int) (int, []int) {
+	var min int
+	minK := []int{}
 	min = arr[0]
 
 	for k, v := range arr {
 		if v == 0 {
 			continue
 		}
-		if v < min {
+		if v < min || (v > min && min == 0) {
 			min = v
-			minK = k
+			minK = []int{}
+			minK = append(minK, k)
+		} else if v == min {
+			min = v
+			minK = append(minK, k)
 		}
 	}
 
-	t := append(arr[:minK], 0)
-	return min, minK, append(t, arr[minK+1:]...)
+	return min, minK
 }
 
-// func findSubArr() {
-// }
+func findEdge(A []int, pos int) (left, right int) {
+
+	left = pos
+	right = pos
+	var p int
+
+	got0 := false
+	for p = pos; p >= 0; p-- {
+		if A[p] == 0 {
+			// left = p
+			got0 = true
+			break
+		}
+		left = p
+	}
+
+	if p == 0 && !got0 {
+		left = 0
+	}
+
+	for p = pos; p < len(A); p++ {
+		if A[p] == 0 {
+			// right = p
+			break
+		}
+		right = p
+	}
+	if p == len(A)-1 {
+		right = len(A) - 1
+	}
+
+	return
+}
+
+func calc(pos, left, right, m int) int {
+	return m * ((pos - left) * (right - pos + 1))
+}
 
 func sumSubarrayMins(A []int) int {
 
-	src := A
+	ret := 0
+	for {
+		m, mPoses := mins(A)
 
-	data := make(map[int]int)
-	for _, v := range A {
-		m, mPos, src := minAndModify(src)
-
-		data[m] = data[m] + 1
-		// src = append(src[
-
-		if len(src) == 0 {
+		if m == 0 {
 			break
+		}
+
+		for _, pos := range mPoses {
+			left, right := findEdge(A, pos)
+			ret += calc(pos, left, right, m)
+		}
+
+		for _, pos := range mPoses {
+			A[pos] = 0
 		}
 	}
 
-	return 0
+	return ret
 }
--- 907_sum-of-subarray-minimums/main_test.go
+++ 907_sum-of-subarray-minimums/main_test.go
@@ -8,13 +8,65 @@ import (
 
 func TestMain(t *testing.T) {
 
-	// assert.Equal(t, "0", multiply("123", "0"))
+	assert.Equal(t, 17, sumSubarrayMins([]int{3, 1, 2, 4}))
 }
 
 func TestFunc(t *testing.T) {
 
-	m, p, a := minAndModify([]int{7, 6, 3, 0, 4, 5})
-	assert.Equal(t, 3, m)
-	assert.Equal(t, 2, p)
-	assert.Equal(t, []int{7, 6, 0, 0, 4, 5}, a)
+	// m, p, a := minAndModify([]int{7, 6, 3, 0, 4, 5})
+	// assert.Equal(t, 3, m) // assert.Equal(t, 2, p)
+	// assert.Equal(t, []int{7, 6, 0, 0, 4, 5}, a)
+}
+
+func TestMins(t *testing.T) {
+
+	var m int
+	var poses []int
+	// m, poses = mins([]int{5, 2, 3, 2, 4})
+	// assert.Equal(t, 2, m)
+	// assert.Equal(t, []int{1, 3}, poses)
+
+	// m, poses = mins([]int{5, 2, 0, 2, 4})
+	// assert.Equal(t, 2, m)
+	// assert.Equal(t, []int{1, 3}, poses)
+
+	// m, poses = mins([]int{0, 2, 0, 2, 0})
+	// assert.Equal(t, 2, m)
+	// assert.Equal(t, []int{1, 3}, poses)
+
+	// m, poses = mins([]int{2})
+	// assert.Equal(t, 2, m)
+	// assert.Equal(t, []int{0}, poses)
+
+	// m, poses = mins([]int{2, 0})
+	// assert.Equal(t, 2, m)
+	// assert.Equal(t, []int{0}, poses)
+
+	m, poses = mins([]int{3, 1, 2, 4})
+	assert.Equal(t, 1, m)
+	assert.Equal(t, []int{1}, poses)
+
+}
+
+func TestEdge(t *testing.T) {
+	var left, right int
+	// left, right = findEdge([]int{5, 2, 3, 2, 4}, 1)
+	// assert.Equal(t, 0, left)
+	// assert.Equal(t, 4, right)
+
+	// left, right = findEdge([]int{0, 5, 2, 3, 2, 0, 4}, 1)
+	// assert.Equal(t, 1, left)
+	// assert.Equal(t, 4, right)
+
+	// left, right = findEdge([]int{0}, 0)
+	// assert.Equal(t, 0, left)
+	// assert.Equal(t, 0, right)
+
+	left, right = findEdge([]int{3, 1, 2, 4}, 1)
+	assert.Equal(t, 0, left)
+	assert.Equal(t, 3, right)
+}
+
+func TestCalc(t *testing.T) {
+	assert.Equal(t, 6, calc(1, 0, 3, 1))
 }
