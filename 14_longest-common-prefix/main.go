11854e995fb4f66e7395e1259511cba34a7e85c2
jinleileiking <jinleileiking@gmail.com>
2019-05-14 14:29:47 +0800

    :tada: 14:done with goroutine

--- 14_longest-common-prefix/main.go
+++ 14_longest-common-prefix/main.go
@@ -1,19 +1,25 @@
 package main
 
-import "sync"
+import (
+	"sync"
+)
 
 var wg sync.WaitGroup
 
 func longestCommonPrefix(strs []string) string {
 
 	// corner case
+	if len(strs) == 0 {
+		return ""
+	}
 
 	if len(strs) == 1 {
 		return strs[0]
 	}
 
+	parsed := strs
 	for {
-		parsed := parse(strs)
+		parsed = parse(parsed)
 
 		if len(parsed) == 1 {
 			return parsed[0]
@@ -25,12 +31,13 @@ func longestCommonPrefix(strs []string) string {
 func parse(strs []string) []string {
 
 	parsed := []string{}
-	for p := 0; p <= len(strs)-1; p += 2 {
+	for p := 0; p <= len(strs)-1-len(strs)%2; p += 2 {
+		str2 := strs[p : p+2]
 		wg.Add(1)
-		go func() {
-			parsed = append(parsed, parsePre(strs[p:p+1]))
+		go func([]string) {
+			parsed = append(parsed, parsePre(str2))
 			wg.Done()
-		}()
+		}(str2)
 	}
 
 	if len(strs)%2 != 0 {
@@ -44,9 +51,16 @@ func parse(strs []string) []string {
 }
 
 func parsePre(str2 []string) string {
+	if len(str2) > 2 {
+		panic(0)
+	}
+
 	var ret string
-	for k, v := range str2[0] {
-		if k <= len(str2)-1 && byte(v) == str2[1][k] {
+	str0 := str2[0]
+	str1 := str2[1]
+
+	for k, v := range str0 {
+		if k <= len(str1)-1 && byte(v) == str1[k] {
 			ret = ret + string(v)
 		} else {
 			break
--- 14_longest-common-prefix/main_test.go
+++ 14_longest-common-prefix/main_test.go
@@ -8,6 +8,16 @@ import (
 
 func TestMain(t *testing.T) {
 
-	assert.Equal(t, "aaa", longestCommonPrefix([]string{"aaab", "aaac"}))
+	// assert.Equal(t, "aaa", longestCommonPrefix([]string{"aaab", "aaac"}))
+	// assert.Equal(t, "aa", longestCommonPrefix([]string{"aa", "aaa"}))
+	// assert.Equal(t, "aa", longestCommonPrefix([]string{"aaab", "aaac", "aaaa", "aabb"}))
+	// assert.Equal(t, "fl", longestCommonPrefix([]string{"flower", "flow", "flight"}))
+	assert.Equal(t, "", longestCommonPrefix([]string{}))
+
+}
+
+func TestParse(t *testing.T) {
+
+	assert.Equal(t, []string{"aa"}, parse([]string{"aaa", "aa"}))
 
 }
